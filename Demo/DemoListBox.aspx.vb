bffb37a4ddb488da65f86b7cada6656e8f0f8511
dungdc40 <kiemt95@gmail.com>
2016-03-25 14:18:16 +0700

    Update DemoListBox.aspx.vb
    aloha

--- Demo/DemoListBox.aspx.vb
+++ Demo/DemoListBox.aspx.vb
@@ -1,34 +1,19 @@
 ﻿Public Class DemoListBox
     Inherits System.Web.UI.Page
     
-    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim str As String = " "
-        For Each itm In ListBox1.Items
-            If itm.Selected Then
-                str = str & itm.Text
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
 
-            End If
+    End Sub
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
 
-        Next
-        Label1.Text = str
+    End Sub
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
     End Sub
     
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not IsPostBack Then
-            Dim q = From f In System.Drawing.FontFamily.Families Select f.Name
-            ListBox1.DataSource = q
-            ListBox1.DataBind()
-        End If
-    End Sub
-    
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If Not IsPostBack Then
-            Dim q = From f In System.Drawing.FontFamily.Families Select f.Name
-            ListBox1.DataSource = q
-            ListBox1.DataBind()
-        End If
-    End Sub
 
     Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
 
@@ -44,5 +29,16 @@
 
         Next
         Label1.Text = str
+    End Sub
+    
+    
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+    Protected Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
     End Sub
 End Class

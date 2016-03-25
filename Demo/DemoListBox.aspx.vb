5786373b93b4443d7de62ab79a0e592ff474fc35
dungdc40 <kiemt95@gmail.com>
2016-03-25 14:16:28 +0700

    Update DemoListBox.aspx.vb

--- Demo/DemoListBox.aspx.vb
+++ Demo/DemoListBox.aspx.vb
@@ -1,5 +1,26 @@
 ﻿Public Class DemoListBox
     Inherits System.Web.UI.Page
+    
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim str As String = " "
+        For Each itm In ListBox1.Items
+            If itm.Selected Then
+                str = str & itm.Text
+
+            End If
+
+        Next
+        Label1.Text = str
+    End Sub
+    
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            Dim q = From f In System.Drawing.FontFamily.Families Select f.Name
+            ListBox1.DataSource = q
+            ListBox1.DataBind()
+        End If
+    End Sub
+    
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not IsPostBack Then
